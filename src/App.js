cfdd31505687112a534c62eef1faf2bf672cf29d
Abelabebe313 <abelabebe313@gmail.com>
2022-12-04 21:44:13 +0300

    New updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Home from "./components/Home";
+import About from "./components/About";
 function App() {
   return (
     <div>
       <Home/>
+      <About/>
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+const About = () => {
+  return (
+    <div className="About-container">
+        <div className="about-left">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Magnam nostrum eaque magni numquam voluptate, cumque itaque at 
+                voluptatibus velit adipisci facilis, ipsam repudiandae tempore 
+                eligendi maxime voluptas repellendus expedita deleniti?
+            </p>
+        </div>
+        <div className="about-right">
+
+        </div>
+    </div>
+  )
+}
+
+export default About
