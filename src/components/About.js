ce2d6c2c76b73b54bc0309c9ae2b3ca6e3b51675
Abelabebe313 <abelabebe313@gmail.com>
2022-12-05 05:16:27 +0300

    New updates

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,22 @@
+import AboutImage from '../assets/programming_re_kg9v.svg'
 const About = () => {
   return (
-    <div className="About-container">
+    <div className="About-page">
+        <div className="About-container">
         <div className="about-left">
+             <img src={AboutImage}/>
+        </div>
+        <div className="about-right">
+            <p className="about-header">About Me</p>
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                 Magnam nostrum eaque magni numquam voluptate, cumque itaque at 
                 voluptatibus velit adipisci facilis, ipsam repudiandae tempore 
                 eligendi maxime voluptas repellendus expedita deleniti?
             </p>
+            <button className="btn">Download CV</button>
         </div>
-        <div className="about-right">
-
-        </div>
+    </div>
     </div>
   )
 }
---
+++ src/components/Skills.js
@@ -0,0 +1,12 @@
+const Skills = () => {
+  return (
+    <div className="skill-page">
+        <p className="skill-header">SkillS</p>
+        <div className="Skill-container">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Skills
